959c7027c4c5b9686978414194bab6848597dc4b
vplauzon <vincent_lauzon@yahoo.com.au>
2022-01-14 10:29:45 -0500

    Sample of cluster + user managed identity

---
+++ cluster-user-id/README.md
@@ -0,0 +1,4 @@
+# Cluster & User Managed identity
+
+Azure Data Explorer cluster referencing a user managed identity.
+
---
+++ cluster-user-id/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##########################################################################
+##  Deploys template
+##
+##  Parameters:
+##
+##  1- Name of resource group
+
+rg=$1
+
+echo "Resource group:  $rg"
+
+echo
+echo "Deploying ARM template"
+
+az deployment group create -n "deploy-$(uuidgen)" -g $rg \
+    --template-file deploy.json
